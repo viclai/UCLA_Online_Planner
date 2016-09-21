4dbbb82e7df596c2ff893d645e9da4788d4da582
viclai <victor1.lai@gmail.com>
2016-09-21 11:24:06 -0700

    Fixed Trace Error

--- UCLA_Student_Planner/index.aspx.cs
+++ UCLA_Student_Planner/index.aspx.cs
@@ -179,7 +179,7 @@ namespace UCLA_Student_Planner
 
             string[] academYearEndContent = 
                 academYearEnd.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            System.Diagnostics.Trace.TraceInformation("Academic End Date: " + academYearEndContent + "\n");
+            System.Diagnostics.Trace.TraceInformation("Academic End Date: " + academYearEnd + "\n");
             foreach (string c in academYearEndContent)
             {
                 System.Diagnostics.Trace.TraceInformation(c + "\n");
